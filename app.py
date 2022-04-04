27d35c592ba99102694e25fd2d6d99c4f56cde87
Francesco Forte <francesco.forte831tech@gmail.com>
2022-04-04 15:53:03 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+
+if __name__ == '__main__':
+    app.run(debug=True) #debug=True: refreshes page every time a change is made. turned off in production 
+    
