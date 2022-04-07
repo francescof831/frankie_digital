4abffb85c529d261849a2d3a7ec573eb6f4924b8
Francesco Forte <francesco.forte831tech@gmail.com>
2022-04-07 17:31:59 -0400

    added some pics and favicon

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, send_from_directory
 import logging
-logging.basicConfig(format = "[%(levelname)s] - %(asctime)s - %(message)s", level=logging.DEBUG, handlers = [logging.StreamHandler(), logging.FileHandler("log.txt")])
-logging.debug("Test <^_^>")
+#logging.basicConfig(format = "[%(levelname)s] - %(asctime)s - %(message)s", level=logging.DEBUG, handlers = [logging.StreamHandler(), logging.FileHandler("log.txt")])
+#logging.debug("Application started")
 
 app = Flask(__name__)
 
@@ -13,6 +14,11 @@ def index():
 def blog():
     return render_template('blog.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0') #debug=True: refreshes page every time a change is made. turned off in production 
+    app.run(host='0.0.0.0',debug=True) #debug=True: refreshes page every time a change is made. turned off in production 
     
